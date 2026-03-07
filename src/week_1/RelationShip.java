828c9a698a413c623187bc6116fa0902c3f44d69
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-03-07 21:23:59 +0700

    first commit

---
+++ src/week_1/RelationShip.java
@@ -0,0 +1,15 @@
+package week_1;
+
+import java.util.ArrayList;
+
+public class RelationShip  {
+    public static void main(String[] args) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("Huynh");
+        arr.add("Phuc");
+        arr.add("Thinh");
+
+        System.out.println(arr);
+
+    }
+}
