b58f4ecc8ebd8391fc537f5b81eb09729e21e32c
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-03-08 00:54:21 +0700

    Demmo aggregation

---
+++ src/week_1/AggregationDemo.java
@@ -0,0 +1,31 @@
+package week_1;
+
+public class AggregationDemo {
+    public static void main(String[] args) {
+        // Creating independent Student objects
+        Student s1 = new Student(22, "01", "Anh Nam");
+        Student s2 = new Student(22, "02", "Vinh Hoa");
+        Student s3 = new Student(22, "03", "My Khanh");
+        Student s4 = new Student(22, "04", "Thinh Huynh");
+
+        // Creating Department object with a Students list
+        // using Aggregation (Department "has" students)
+
+        Deparment sec = new Deparment("SEC");
+        sec.addStudent(s1);
+        sec.addStudent(s2);
+
+        Deparment ev = new Deparment("EV");
+        ev.addStudent(s4);
+        ev.addStudent(s3);
+
+        // Creating an Institute object with Departments list
+        // using Aggregation (Institute "has" Departments)
+        Institute ins = new Institute("VBDD");
+        ins.addDepartment(sec);
+        ins.addDepartment(ev);
+
+        System.out.println("Total student: " + ins.getTotalStudent());
+
+    }
+}
