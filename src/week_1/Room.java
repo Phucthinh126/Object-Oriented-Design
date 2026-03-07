0d748deaecaf7eb8cbe024c8e20032b22bfde72a
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-03-08 01:09:59 +0700

    class room

--- src/week_1/Room.java
+++ src/week_1/Room.java
@@ -1,4 +1,13 @@
 package week_1;
 
 public class Room {
+    private String roomName;
+
+    public Room(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
 }
