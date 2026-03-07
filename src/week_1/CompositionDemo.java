edd664dd4f939fa68e995b6516b19f7df9519cc1
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-03-08 01:27:16 +0700

    Update

--- src/week_1/CompositionDemo.java
+++ src/week_1/CompositionDemo.java
@@ -1,4 +1,19 @@
 package week_1;
 
 public class CompositionDemo {
+    public static void main(String[] args) {
+        House house = new House("Dream House");
+
+        house.addRoom(new Room("Living Room"));
+        house.addRoom(new Room("Bedroom"));
+        house.addRoom(new Room("Kitchen"));
+        house.addRoom(new Room("Bathroom"));
+        int r = house.getTotalRooms();
+        System.out.println("Total Rooms: " + r);
+
+        System.out.println("Room names: ");
+        for (Room room : house.getRooms()) {
+            System.out.println("- " + room.getRoomName());
+        }
+    }
 }
