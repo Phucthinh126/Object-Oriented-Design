05c0cbb6cb4a65f9153d93ec3926b2e3fab0d46c
Huynh Phuc Thinh <phucthinhlap100323@gmail.com>
2026-03-12 00:47:07 +0700

    Realizition relationship

--- src/week_1/Drawable.java
+++ src/week_1/Drawable.java
@@ -1,4 +1,17 @@
 package week_1;
-
+// Realization
+// Interface  - bản thiết kế
 public interface Drawable {
+    // Constants
+    String DEFAUT_COLER = "black";
+
+    // Abstract methods
+    void draw();
+    void resize(double factor);
+
+    // default method (java 8+)
+    default void display(){
+        System.out.println("Displaying with coler: " + geColer());
+
+    }
 }
